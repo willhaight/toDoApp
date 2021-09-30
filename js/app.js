b5215166717024819bf086834a812c374d766842
Will Haight <willhaight121@gmail.com>
2021-09-30 08:53:51 -0600

    creat a new card and logos

--- js/app.js
+++ js/app.js
@@ -1,2 +1,19 @@
-x = 5 
-console.log(x)
+//add card setup
+
+const createCard = document.getElementById('addCard');
+let recCardName = document.getElementById('cardName');
+let cardStore = document.getElementById('cardOp');
+let make = 0;
+
+//creating a card
+
+createCard.onclick = function(){
+    make++;
+    addCard();
+}
+
+function addCard(){
+    cardStore.innerHTML += "<div id=card" + make + "><i class='fas fa-sticky-note'>" +
+    "</i><span>" + recCardName.value + "</span></div>"
+}
+
