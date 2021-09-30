bb7b1c87230cc08c40d25dbacb5b5e9e3c0a5677
Will Haight <willhaight121@gmail.com>
2021-09-30 10:09:33 -0600

    fixed height issue. Array Targeting is off

--- js/app.js
+++ js/app.js
@@ -4,6 +4,8 @@ const createCard = document.getElementById('addCard');
 let recCardName = document.getElementById('cardName');
 let cardStore = document.getElementById('cardOp');
 let make = 0;
+let cardNum = [];
+let cardListAdd;
 
 //creating a card
 
@@ -13,7 +15,13 @@ createCard.onclick = function(){
 }
 
 function addCard(){
-    cardStore.innerHTML += "<div id=card" + make + "><i class='fas fa-sticky-note'>" +
-    "</i><span>" + recCardName.value + "</span></div>"
+    cardStore.innerHTML += "<div id='card" + make + 
+    "'><i class='fas fa-sticky-note'>" +
+    "</i><span>" + recCardName.value + "</span></div>";
+    cardListAdd = document.getElementById('card' + make);
+    cardNum.push(cardListAdd);
+    recCardName.value = '';
 }
+
+//Listing in a card
 
