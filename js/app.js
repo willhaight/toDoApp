4b3244054ba19d073ba1fccaa73ecb754f4b3cad
Will Haight <willhaight121@gmail.com>
2021-10-11 08:20:36 -0600

    lots of changes

--- js/app.js
+++ js/app.js
@@ -2,26 +2,83 @@
 
 const createCard = document.getElementById('addCard');
 let recCardName = document.getElementById('cardName');
-let cardStore = document.getElementById('cardOp');
-let make = 0;
-let cardNum = [];
-let cardListAdd;
+let currentCards = [];
+let cardNum = [{
+    id: 0,
+    name: "card"
+},];
+const listsContainer = document.querySelector('#cardOp');
+let cardData = document.getElementById('cardData');
+
 
 //creating a card
 
 createCard.onclick = function(){
-    make++;
-    addCard();
+cardNum.forEach(list => {
+const listElement = document.createElement('div')
+listElement.classList.add('card')
+listElement.innerHTML = '<i class="fas fa-sticky-note"></i>' + recCardName.value + '<input type="button" value="delete" class="delete">';
+listsContainer.appendChild(listElement);
+currentCards.push(listElement);
+savetoStorage();
+loadUp();
+})
+recCardName.value = "";
 }
 
-function addCard(){
-    cardStore.innerHTML += "<div id='card" + make + 
-    "'><i class='fas fa-sticky-note'>" +
-    "</i><span>" + recCardName.value + "</span></div>";
-    cardListAdd = document.getElementById('card' + make);
-    cardNum.push(cardListAdd);
-    recCardName.value = '';
+//Local Storage
+
+if(localStorage.getItem('cards')){
+    listsContainer.innerHTML = localStorage.getItem('cards');
 }
 
-//Listing in a card
+for(let i = 0; i < document.getElementsByClassName('card')["length"]; i++){
+    currentCards.push(document.getElementsByClassName('card')[i])
+    }
 
+
+function savetoStorage(){
+    localStorage.setItem('cards', listsContainer.innerHTML);
+}
+
+//list editing
+
+let storedListData = [];
+
+
+//List data in local storage
+
+let dataNum = 0;
+
+
+function dataStorageSave(){
+localStorage.setItem('cardData' + dataNum, cardData.innerHTML);
+}
+
+//creating the list data functions
+
+
+function loadUp(){
+    dataNum = 0;
+for(let i = 0; i < currentCards.length; i++){
+    currentCards[i].onclick = function(){
+        dataNum = i;
+        currentCards[i].classList.add('selected');
+        selection();
+    }
+
+    //deleting the lists
+
+    document.getElementsByClassName('delete')[i].onclick = function() {
+        listsContainer.innerHTML = null;
+        currentCards = [];
+        savetoStorage();
+    }
+}}
+loadUp();
+
+function selection() {
+    cardData.innerHTML = '<h1>' + currentCards[dataNum].innerText + '</h1>';
+    currentCards[dataNum].classList.remove('selected');
+
+}
