19f0ef0552a161fd0810775fa432340935a99641
Will Haight <willhaight121@gmail.com>
2021-10-12 09:38:02 -0600

    functioning perfectly

--- js/app.js
+++ js/app.js
@@ -17,7 +17,7 @@ createCard.onclick = function(){
 cardNum.forEach(list => {
 const listElement = document.createElement('div')
 listElement.classList.add('card')
-listElement.innerHTML = '<i class="fas fa-sticky-note"></i>' + recCardName.value + '<input type="button" value="delete" class="delete">';
+listElement.innerHTML = '<i class="fas fa-sticky-note"></i>' + recCardName.value + '<input style="display:none;" type="button" value="delete" class="delete">';
 listsContainer.appendChild(listElement);
 currentCards.push(listElement);
 savetoStorage();
@@ -56,15 +56,19 @@ function dataStorageSave(){
 localStorage.setItem('cardData' + dataNum, cardData.innerHTML);
 }
 
-//creating the list data functions
+//creating the list data functions and selecting a container
 
 
 function loadUp(){
     dataNum = 0;
 for(let i = 0; i < currentCards.length; i++){
     currentCards[i].onclick = function(){
+        for(let i = 0; i < document.getElementsByClassName('delete').length;i++){
+            document.getElementsByClassName('delete')[i].style.display = "none";
+        }
         dataNum = i;
         selection();
+        showDelete(i);
     }
 
     //deleting the lists
@@ -80,6 +84,7 @@ loadUp();
 
 function checkDelete(deleteNum){
     if(localStorage.getItem('cardData' + deleteNum) == "deleted" || document.getElementsByClassName('data').length == 0){
+        console.log('deleted')
         currentCards[deleteNum].style.display = 'none';
     }else{
         alert('please empty the list before deleting the container')
@@ -88,6 +93,7 @@ function checkDelete(deleteNum){
 
 
 function selection() {
+    console.log('genereated')
     cardData.innerHTML = '<h1>' + currentCards[dataNum].innerText + '</h1>' +
     '<input type=text id="listName"> <input type=button value="Add List Item" id="submitData">';
     if(localStorage.getItem('cardData' + dataNum)){
@@ -95,7 +101,7 @@ function selection() {
     }else{
         dataStorageSave();
     }
-
+    
 
     for(let i = 0; i < document.getElementsByClassName('data').length; i++){
         document.getElementsByClassName('dataDelete')[i].onclick = function(){
@@ -121,4 +127,8 @@ function selection() {
     }
 
 
+}
+
+function showDelete(check){
+    document.getElementsByClassName('delete')[check].style.display = 'block';
 }
